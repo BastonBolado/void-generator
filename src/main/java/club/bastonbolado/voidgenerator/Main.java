8d20e1818a6c5299195c9c48eae67539f08bccd9
shirokuro <46kuro212@gmail.com>
2020-03-15 21:07:24 +0900

    Remove Nonnull annotation
    Annotation Nonnull is not exists (Maybe provided by IDE)

--- src/main/java/club/bastonbolado/voidgenerator/Main.java
+++ src/main/java/club/bastonbolado/voidgenerator/Main.java
@@ -4,8 +4,6 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import javax.annotation.Nonnull;
 import java.util.Random;
 import java.util.logging.Level;
 
@@ -23,19 +21,16 @@ public class Main extends JavaPlugin {
     public class EmptyChunkGenerator extends ChunkGenerator {
 
         @Override
-        @Nonnull
-        public ChunkData generateChunkData(@Nonnull World world, @Nonnull Random random, int x, int z, @Nonnull BiomeGrid biome) {
+        public ChunkData generateChunkData(World world, Random random, int x, int z, BiomeGrid biome) {
             return createChunkData(world);
         }
 
         @Override
-        @Nonnull
-        public boolean canSpawn(@Nonnull World world, int x, int z) {
+        public boolean canSpawn(World world, int x, int z) {
             return true;
         }
 
         @Override
-        @Nonnull
         public Location getFixedSpawnLocation(World world, Random random) {
             return new Location(world, 0.0D, 64.0D, 0.0D);
         }
