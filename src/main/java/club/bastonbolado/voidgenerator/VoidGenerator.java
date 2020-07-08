28a2deb844d77d8bd55dcf2cdc3cba962d79702e
lokka30 <lokka30@protonmail.com>
2020-07-08 19:40:55 +0800

    Renamed main class
    Set api-version to 1.13 to add compatibility with 1.13 and 1.14 too (no detriment to the plugin)
    
    Increment version 1.0 -> 1.1

--- src/main/java/club/bastonbolado/voidgenerator/VoidGenerator.java
+++ src/main/java/club/bastonbolado/voidgenerator/VoidGenerator.java
@@ -3,7 +3,7 @@ package club.bastonbolado.voidgenerator;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class VoidGenerator extends JavaPlugin {
 
     public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
         return new EmptyChunkGenerator();
